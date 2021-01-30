b0267eeda376b709006278167d4101eb241dd5f7
Michael Stromer <>
2021-01-30 15:49:03 -0500

    update remote

--- shared/src/commonMain/kotlin/com/example/justdesserts/shared/ApolloProvider.kt
+++ shared/src/commonMain/kotlin/com/example/justdesserts/shared/ApolloProvider.kt
@@ -12,7 +12,7 @@ class ApolloProvider(databaseDriverFactory: DatabaseDriverFactory) : TokenProvid
     internal val database = Database(databaseDriverFactory)
     internal val apolloClient: ApolloClient = ApolloClient(
         networkTransport = ApolloHttpNetworkTransport(
-            serverUrl = "http://localhost:8080/graphql",
+            serverUrl = "https://ktor-graphql.herokuapp.com/graphql",
             headers = mapOf(
                 "Accept" to "application/json",
                 "Content-Type" to "application/json",
